6c2e28eb94271e7d2786816908c5519a98f86dc6
abdulbari149 <abdulbari131103@gmail.com>
2023-07-25 01:45:42 +0500

    feat: body parsing, cors and static files middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,30 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
+import cors from 'cors';
 import morgan from './config/morgan';
 
 const app = express();
 
-app.use(morgan.successHandler)
-app.use(morgan.errorHandler)
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// serving static images
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+// morgan for api logging
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
+
+// enable cors
+app.use(cors());
 
 app.get('/', (_req: Request, res: Response) => {
-  const message = 'Error: Occured'
-  res.locals.errorMessage = message
+  const message = 'Error: Occured';
+  res.locals.errorMessage = message;
   res.status(500).send(message);
 });
-
 
 export default app;
